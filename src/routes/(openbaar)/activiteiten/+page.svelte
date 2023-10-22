635f8f537e064a09ac0c02c1e18652a063a28813
Quinten Libert <libert1quinten@gmail.com>
2023-10-22 20:22:05 +0200

    Oplossen cosmetisch foutje activiteiten

--- src/routes/(openbaar)/activiteiten/+page.svelte
+++ src/routes/(openbaar)/activiteiten/+page.svelte
@@ -11,22 +11,35 @@
 			opkomende_activiteiten = data.activiteiten.filter((a: any) => {
 				return new Date(a.datum).getTime() >= new Date().getTime();
 			});
-			afgelopen_activiteiten = data.activiteiten.filter((a: any) => {
-				return new Date(a.datum).getTime() < new Date().getTime();
-			}).reverse();
+			afgelopen_activiteiten = data.activiteiten
+				.filter((a: any) => {
+					return new Date(a.datum).getTime() < new Date().getTime();
+				})
+				.reverse();
 		}
 	}
 </script>
 
 <svelte:head>
 	<title>Activiteiten</title>
-	<meta name="description" content="Een lijst van de geplande en afgelopen activiteiten georganiseerd door de Homeraad Vermeylen.">
+	<meta
+		name="description"
+		content="Een lijst van de geplande en afgelopen activiteiten georganiseerd door de Homeraad Vermeylen."
+	/>
 </svelte:head>
 
 <div class="flex flex-col min-h-[calc(100vh-64px)] gap-2 items-center">
 	<h1 class="text-center text-3xl pt-4 font-bold mb-5 mt-5">Opkomende activiteiten</h1>
-	{#if opkomende_activiteiten.length != 0}
-		<div class="flex flex-col gap-2 items-center md:flex-none md:grid md:grid-cols-2 md:items-baseline">
+	{#if opkomende_activiteiten.length > 1}
+		<div
+			class="flex flex-col gap-2 items-center md:flex-none md:grid md:grid-cols-2 md:items-baseline"
+		>
+			{#each opkomende_activiteiten as activiteit (activiteit.id)}
+				<Activiteit {activiteit} />
+			{/each}
+		</div>
+	{:else if opkomende_activiteiten.length == 1}
+		<div class="items-center">
 			{#each opkomende_activiteiten as activiteit (activiteit.id)}
 				<Activiteit {activiteit} />
 			{/each}
@@ -34,14 +47,22 @@
 	{:else}
 		<h1>Er zijn voorlopig geen activiteiten gepland.</h1>
 	{/if}
-		<h1 class="text-center text-3xl font-bold pt-4 mb-5">Afgelopen activiteiten</h1>
-			{#if afgelopen_activiteiten.length != 0}
-				<div class="flex flex-col gap-2 items-center md:flex-none md:grid md:grid-cols-2 md:items-baseline">
-					{#each afgelopen_activiteiten as activiteit (activiteit.id)}
-						<Activiteit {activiteit} />
-					{/each}
-				</div>
-			{:else}
-				<h1>Niets om weer te geven.</h1>
-			{/if}
+	<h1 class="text-center text-3xl font-bold pt-4 mb-5">Afgelopen activiteiten</h1>
+	{#if afgelopen_activiteiten.length > 1}
+		<div
+			class="flex flex-col gap-2 items-center md:flex-none md:grid md:grid-cols-2 md:items-baseline"
+		>
+			{#each afgelopen_activiteiten as activiteit (activiteit.id)}
+				<Activiteit {activiteit} />
+			{/each}
+		</div>
+	{:else if afgelopen_activiteiten.length == 0}
+		<div class="items-center">
+			{#each afgelopen_activiteiten as activiteit (activiteit.id)}
+				<Activiteit {activiteit} />
+			{/each}
+		</div>
+	{:else}
+		<h1>Niets om weer te geven.</h1>
+	{/if}
 </div>
