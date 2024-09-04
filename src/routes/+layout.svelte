9aad88e89ec75b4487109b3a882b190174049852
Quinten Libert <libert1quinten@gmail.com>
2024-09-04 20:49:31 +0200

    Toevoegen umami analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,22 @@
 <script>
-	import { Toaster } from "$lib/components/ui/sonner";
-	
-	export let data;
+	import { Toaster } from '$lib/components/ui/sonner';
 
+	export let data;
 </script>
 
 <svelte:head>
-  <meta charset="utf8" />
+	<meta charset="utf8" />
+	{#if import.meta.env.PROD}
+		<script
+			defer
+			src="https://cloud.umami.is/script.js"
+			data-website-id="a628ad5e-8f36-43aa-8215-374451f852df"
+		></script>
+	{/if}
 </svelte:head>
 
 <Toaster richColors theme="dark" />
 
 {#key data.url}
-    <slot />
+	<slot />
 {/key}
